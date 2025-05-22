4e0a2fdef3976b9c7b78cb8da482380102dca65b
zhangkeyu03 <1453150422@qq.com>
2025-05-22 15:46:27 +0800

    Update 2_insert_test_data.sql

--- sql/2_insert_test_data.sql
+++ sql/2_insert_test_data.sql
@@ -1,13 +1,13 @@
 
--- 插入分析项目
+-- Вставьте проект анализа
 INSERT INTO analiz (analiz_name)
 SELECT 'Analiz_' || i FROM generate_series(1, 100) AS i;
 
--- 插入员工信息
+-- Вставьте информацию о сотруднике
 INSERT INTO employee (full_name)
 SELECT 'Employee_' || i FROM generate_series(1, 50) AS i;
 
--- 插入 50,000 条订单
+-- Введите 50 000 заказов
 INSERT INTO works (date_created, patient_name, is_deleted)
 SELECT 
     NOW() - (random() * INTERVAL '365 days'),
@@ -15,7 +15,7 @@ SELECT
     CASE WHEN random() > 0.05 THEN FALSE ELSE TRUE END
 FROM generate_series(1, 50000);
 
--- 插入 150,000 条订单项
+-- Вставьте 150 000 позиций
 INSERT INTO workitem (work_id, analiz_id, employee_id)
 SELECT 
     ((g - 1) / 3 + 1) AS work_id,
